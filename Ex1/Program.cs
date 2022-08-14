c6aaaa6c1092c1956725d9a9ff5be63383eb37b6
Andrey <this.is.ya8@gmail.com>
2022-08-14 13:01:03 +1000

    C.Sharp-DZ3

---
+++ Ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+//string a = "14212";
+string a = "12821";
+//string a = "23432";
+
+char[] x = a.ToCharArray();
+Array.Reverse(x);
+string b = string.Join("", x);
+System.Console.WriteLine($"value: {a == b}");
